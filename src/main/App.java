3e6076768f5f40cfca984475e44917e990b4aef5
EugeniVoitehovski <33103080+EugeniVoitehovski@users.noreply.github.com>
2021-04-23 18:04:35 +0300

    Basic interface

---
+++ src/main/App.java
@@ -0,0 +1,30 @@
+package main;
+
+import mails.mail;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App {
+    private JButton mail;
+    private JPanel panMain;
+
+    public App() {
+        mail.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mails.mail mailMainWindow = new mail();
+                mailMainWindow.open();
+            }
+        });
+    }
+
+    public static void main (String[] args) {
+        JFrame frame = new JFrame("Система ТС");
+        frame.setContentPane(new App().panMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}

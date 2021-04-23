ff590308e5f2ec8a310d82c3c25cfba6d612b831
txlyzov <txlyzov@mail.ru>
2021-04-23 20:08:22 +0300

    Mail util + lib

---
+++ src/utils/JavaMailUtil.java
@@ -0,0 +1,49 @@
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+
+
+public class JavaMailUtil {
+
+    public static void sendMail(String recepient,String title,String text) throws Exception {
+        System.out.println("Starting...");
+        Properties properties = new Properties();
+
+        properties.put("mail.smtp.auth","true");
+        properties.put("mail.smtp.starttls.enable","true");
+        properties.put("mail.smtp.host","smtp.gmail.com");
+        properties.put("mail.smtp.port","587");
+
+        String myAccountEmail = "diplomaTestMail@gmail.com";
+        String password = "diplomaMail";
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(myAccountEmail,password);
+            }
+        });
+
+        Message message = prepareMessage(session,myAccountEmail,recepient,title,text);
+        Transport.send(message);
+        System.out.println("Success");
+
+
+    }
+
+    private static Message prepareMessage(Session session,String myAccountEmail,String recepient,String title,String text) {
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(myAccountEmail));
+            message.setRecipient(Message.RecipientType.TO,new InternetAddress(recepient));
+            message.setSubject(title);
+            message.setText(text);
+            return message;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}

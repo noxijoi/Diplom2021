9dadbe112a3c765216afa9133b74503020b5a5d6
txlyzov <txlyzov@mail.ru>
2021-04-24 23:27:42 +0300

    interface update

--- src/main/App.java
+++ src/main/App.java
@@ -1,27 +1,24 @@
 package main;
 
-
-import mails.mail;
-
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class App {
     private JButton mail;
     private JPanel panMain;
+    private JTabbedPane mainTabbedPane;
+    private JPanel mailMainPane;
+    private JButton inmessage;
+    private JButton outmessage;
+    private JButton notificationsWindowButton;
+    private JButton mapWindowButton;
+    private JButton installedSensorsButton;
+    private JComboBox mapFiltersChoose;
+    private JButton knownSensorsButton;
+    private JPanel sensorsPane;
+    private JPanel mainPane;
+    private JLabel mapFilters;
 
-    public App() {
-        mail.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                mails.mail mailMainWindow = new mail();
-                mailMainWindow.open();
-            }
-        });
-    }
-
-    public static void main (String[] args) {
+    public static void main (String[] args){
         JFrame frame = new JFrame("Система ТС");
         frame.setContentPane(new App().panMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
